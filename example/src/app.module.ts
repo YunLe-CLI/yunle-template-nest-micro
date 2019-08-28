3f61400f20660e3e57af29d870be85956e8819e4
大狗吱 <460559761@qq.com>
2019-08-28 11:07:01 +0800

    fix

--- example/src/app.module.ts
+++ example/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MicroModule } from '@micro/example';
+import { MicroModule } from '../../dist';
 
 @Module({
   imports: [MicroModule],
